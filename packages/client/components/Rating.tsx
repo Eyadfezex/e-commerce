0931ab40eeccd7c6cb9e13469aae8c70b8eda0e3
Eyad Ahmed <Eyad932004@outlook.com>
2024-10-13 00:28:44 +0300

    [0028]-edit ProductCard component to read data

--- packages/client/components/Rating.tsx
+++ packages/client/components/Rating.tsx
@@ -1,20 +1,13 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
-import Typography from "@mui/material/Typography";
-
-export default function BasicRating() {
-  const [value, setValue] = React.useState<number | null>(0);
-
+interface PROPS {
+  value: number;
+}
+export default function BasicRating({ value }: PROPS) {
   return (
     <Box sx={{ display: "flex", gap: "0.5rem" }}>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+      <Rating name="simple-controlled" value={value} readOnly />
       <p>
         {value}
         <span className="opacity-70">/5</span>
