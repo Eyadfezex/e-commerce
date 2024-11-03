6c8eca47aa65c348602159d576a95f526187fce1
Eyad Ahmed <Eyad932004@outlook.com>
2024-11-03 11:52:20 +0200

    [0052] Adjust Footer Media Queries for Improved Responsiveness
    - Modified footer media query widths to enhance responsiveness on various screen sizes.
    - Ensured consistent layout and alignment across devices for a polished user experience.

--- packages/client/components/NewArrival.tsx
+++ packages/client/components/NewArrival.tsx
@@ -1,20 +1,31 @@
+"use client";
 import React from "react";
-import NewArrivalCards from "./NewArrivalCards";
+import { ProductCards } from "./ProductCards";
 import { Button } from "@nextui-org/button";
+import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import { getNewArrivalProducts } from "@/axios/Axios";
 const NewArrival = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["new-arrival-products"],
+    queryFn: getNewArrivalProducts,
+  });
+
   return (
-    <section className="font-sans">
+    <section>
       <div className="flex justify-center py-12 lg:py-20">
-        <div className="flex flex-col items-center px-4 w-full lg:w-[100%] max-w-[1920px] gap-8 ">
+        <div className="flex flex-col items-center px-4 w-full lg:w-[95%] xl:w-[80%] max-w-[1920px] gap-8 ">
           <h2 className="font-bebas font-black text-4xl lg:text-5xl uppercase -tracking-wider text-center">
             NEW ARRIVALS
           </h2>
           <div className="overflow-x-scroll w-full">
-            <NewArrivalCards />
+            <ProductCards data={data} error={error} isLoading={isLoading} />
           </div>
-          <Button className="border py-4 rounded-full w-full  max-w-[400px] md:max-w-[200px]">
-            View All
-          </Button>
+          <Link href="/newarrival">
+            <Button className="border py-4 rounded-full w-[200px] ">
+              View All
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
