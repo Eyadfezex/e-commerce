66f8640ec635e535efba4a06dbeaef49aaa77151
Eyad Ahmed <Eyad932004@outlook.com>
2024-11-03 11:44:30 +0200

    [0051] Update Hero Section and Content Components
    - Adjusted media queries for the Hero section to improve responsiveness across devices.
    - Made content modifications to various components for better layout and consistency.
    - Enhanced styling and alignment to ensure a cohesive user experience.

--- packages/client/components/HeroContent.tsx
+++ packages/client/components/HeroContent.tsx
@@ -4,10 +4,10 @@ import { Button } from "@nextui-org/button";
 const HeroContent = () => {
   return (
     <div className="flex flex-col gap-4 md:gap-6 xl:max-w-[40%] lg:pb-12 xl:gap-13">
-      <h1 className="uppercase text-4xl md:text-6xl xl:text-7xl font-black font-bebas -tracking-widest">
-        <span className="block ">FIND CLOTHES</span>
-        <span className="block ">THAT MATCHES</span>
-        <span className="block ">YOUR STYLE</span>
+      <h1 className="uppercase text-4xl md:text-6xl xl:text-7xl  font-black font-bebas -tracking-widest">
+        <span className="block">FIND CLOTHES</span>
+        <span className="block">THAT MATCHES</span>
+        <span className="block">YOUR STYLE</span>
       </h1>
       <p className="text-sm md:text-base text-zinc-500">
         Browse through our diverse range of meticulously crafted garments,
@@ -17,7 +17,7 @@ const HeroContent = () => {
       <Button className="bg-black text-white w-full py-6 rounded-full  max-w-[400px] md:max-w-[200px]">
         Shop Now
       </Button>
-      <div className="flex flex-wrap justify-center md:justify-start gap-[1rem] divide divide-x-2 w-full">
+      <div className="flex flex-wrap justify-center md:justify-start gap-[1rem] md:divide md:divide-x-2 w-full">
         <div className="px-4">
           <span className=" text-2xl font-bold">200+</span>
           <p className="text-sm text-zinc-500">International Brands</p>
