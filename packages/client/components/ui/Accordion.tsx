2b0871d1c6f17bdf6619be5953881a5bb97b6043
Eyad Ahmed <Eyad932004@outlook.com>
2024-11-27 10:25:51 +0200

    [0098] Add documentation to Accordion component Added detailed documentation to the Accordion component, covering its props, usage examples, and behavior. These updates improve code clarity and assist developers in utilizing the component effectively.

--- packages/client/components/ui/Accordion.tsx
+++ packages/client/components/ui/Accordion.tsx
@@ -1,32 +1,53 @@
+/**
+ * Accordion Component
+ *
+ * This component wraps around Material-UI's Accordion component to create a styled collapsible section with a summary (heading) and detailed content.
+ * The accordion can be optionally expanded by default.
+ * It uses a customized Material-UI accordion with styling adjustments for flexibility in various UI contexts.
+ */
+
 import React from "react";
 import MuiAccordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import { FaAngleDown } from "react-icons/fa6";
 
+/**
+ * Accordion Component
+ * @param {React.ReactNode} children - The content to display inside the accordion when expanded.
+ * @param {string} heading - The heading or summary text displayed in the closed state of the accordion.
+ * @param {boolean} [defaultExpanded=false] - Indicates whether the accordion is expanded by default.
+ * @returns {JSX.Element} - A styled accordion component with a collapsible section.
+ */
 const Accordion = ({
   children,
   heading,
+  defaultExpanded = false,
 }: {
   children: React.ReactNode;
   heading: string;
+  defaultExpanded?: boolean;
 }) => {
   return (
     <div>
-      <MuiAccordion className="!shadow-none">
+      {/* Custom-styled Material-UI Accordion */}
+      <MuiAccordion className="!shadow-none" defaultExpanded={defaultExpanded}>
         <AccordionSummary
-          className="!px-0 !min-h-0 !my-0  font-bold text-xl capitalize"
+          className="!px-0 !min-h-0 !my-0 font-bold text-xl capitalize"
           sx={{
+            // Override specific Material-UI styles
             "& .css-eqpfi5-MuiAccordionSummary-content": {
               margin: "0px !important",
             },
           }}
-          expandIcon={<FaAngleDown />}
+          expandIcon={<FaAngleDown />} // Custom icon for expanding/collapsing
           aria-controls="panel1-content"
           id="panel1-header"
         >
           {heading}
         </AccordionSummary>
+
+        {/* Content displayed when the accordion is expanded */}
         <AccordionDetails className="!px-0 !gap-2 !flex !flex-col !w-full">
           {children}
         </AccordionDetails>
