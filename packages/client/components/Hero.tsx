ffb450c07ef91af370b80b41340a8de4256c4360
Eyad Ahmed <Eyad932004@outlook.com>
2024-11-27 10:39:05 +0200

    [0103] Update Hero section model image styles, source, and fetch priority Refined the styles and source for the model image in the Hero section, ensuring it aligns with the overall design. Additionally, adjusted the image's fetch priority to improve loading performance and optimize the user experience.

--- packages/client/components/Hero.tsx
+++ packages/client/components/Hero.tsx
@@ -1,27 +1,27 @@
 import Image from "next/image";
-import model from "@/public/assets/img/pexels-marleneleppanen-1183266.png";
 import gucci from "@/public/assets/img/gucci.png";
 import prada from "@/public/assets/img/prada.png";
 import zara from "@/public/assets/img/zara.png";
 import versace from "@/public/assets/img/versace.png";
 import calvin_klein from "@/public/assets/img/calvin-klein.png";
-
 import React from "react";
 import HeroContent from "./HeroContent";
 import StarParallax from "./ui/StarParallax";
 
 const Hero = () => {
   return (
-    <section className="bg-[#F2F0F1] ">
+    <section className="bg-[#F2F0F1]">
       <div className="flex justify-center pt-[3rem] 2xl:min-h-[720px]">
         <div className="flex flex-col lg:flex-row lg:justify-between items-center lg:items-end xl:items-center w-full lg:w-[95%] xl:w-[80%] px-4 max-w-[1920px] gap-4 md:gap-10 ">
           <HeroContent />
           <div className="relative">
             <Image
-              src={model}
+              src="https://i.ibb.co/cKB9mgC/pexels-marleneleppanen-1183266.webp"
               alt="model"
+              fetchPriority="high"
+              width={2000}
+              height={2000}
               className="w-full max-w-[500px] lg:max-w-[800px] xl:max-w-[600px]"
-              priority
             />
             <StarParallax />
           </div>
