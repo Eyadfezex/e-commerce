ff0ce036f237b386f0cff67bab97321ef67b1089
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-09 23:28:44 +0200

    fix(client): reduce parallax strength for StarParallax component

--- packages/client/components/ui/StarParallax.tsx
+++ packages/client/components/ui/StarParallax.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 const StarParallax = () => {
   return (
     <>
-      <ScrollParallax isAbsolutelyPositioned strength={0.4}>
+      <ScrollParallax isAbsolutelyPositioned strength={0.2}>
         <Image
           src={star}
           alt="star"
           className="absolute top-40 w-[50px] left-[10%]"
         />
       </ScrollParallax>
-      <ScrollParallax isAbsolutelyPositioned strength={0.4}>
+      <ScrollParallax isAbsolutelyPositioned strength={0.2}>
         <Image src={star} alt="star" className="absolute top-0 right-[0%]" />
       </ScrollParallax>
     </>
