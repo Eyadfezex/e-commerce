6dc22977a3a5e43a775402e3bd3685920a425117
Eyad Ahmed <Eyad932004@outlook.com>
2025-01-10 23:33:33 +0200

    feat(client): implement filter component with animation and toggle functionality in AllProducts page

--- packages/client/pages/AllProducts.tsx
+++ packages/client/pages/AllProducts.tsx
@@ -7,10 +7,12 @@ import Products from "@/components/Product/FullProductsCards"; // Component to d
 import ProductsPageLabel from "@/components/ui/ProductsPageLabel"; // Component to display page label
 import { Button } from "@nextui-org/button"; // UI button component
 import { keepPreviousData, useQuery } from "@tanstack/react-query"; // React Query hooks for data fetching
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa6"; // Icons for pagination buttons
+import { FaArrowLeft, FaArrowRight, FaFilter } from "react-icons/fa6"; // Icons for pagination buttons
 import { useRouter } from "next/navigation"; // Router hook for navigation
 import React, { useState, useEffect } from "react"; // React hooks
 import { Pagination } from "@nextui-org/react"; // Pagination component
+import { motion } from "framer-motion";
+import { FaTimes } from "react-icons/fa";
 
 /**
  * Function to update the search parameters in the URL.
@@ -22,28 +24,31 @@ const updateSearchParams = (
   callback?: () => void
 ): void => {
   try {
+    // Get the current search parameters
     const searchParams = new URLSearchParams(window.location.search);
 
     // Update or delete search parameters
     Object.entries(params).forEach(([key, value]) => {
       if (value === null || value === undefined) {
-        searchParams.delete(key);
+        searchParams.delete(key); // Remove parameter
       } else {
-        searchParams.set(key, String(value));
+        searchParams.set(key, String(value)); // Update or add parameter
       }
     });
 
-    // Update the browser's history state
-    window.history.pushState(
-      {},
-      "",
-      `${window.location.pathname}?${searchParams.toString()}`
-    );
+    // Create the new URL
+    const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+    console.log("Updated URL:", newUrl); // Debugging
 
-    // Execute the callback if provided
-    if (callback) callback();
+    // Update the browser's URL without reloading the page
+    window.history.pushState({}, "", newUrl);
+
+    // Execute the callback function if provided
+    if (callback) {
+      callback();
+    }
   } catch (error) {
-    console.error("Failed to update search params:", error);
+    console.error("Error updating search parameters:", error);
   }
 };
 
@@ -54,6 +59,7 @@ const AllProducts = () => {
   const router = useRouter(); // Hook for navigation
   const [page, setPage] = useState(1); // State for current page
   const [limit, setLimit] = useState(10); // State for items per page
+  const [isOpen, setIsOpen] = useState(false);
 
   // Fetch products data using React Query
   const { data, isLoading } = useQuery({
@@ -91,20 +97,45 @@ const AllProducts = () => {
     updateSearchParams({ page: previousPage }, () => setPage(previousPage));
   };
 
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
   return (
     <section>
-      <div className="flex justify-center py-8">
-        <div className="flex w-full lg:w-[95%] xl:w-[85%] max-w-[1920px] gap-5">
+      <div className="flex justify-center pb-8 pt-[12px]">
+        <div className="flex w-full px-4 lg:w-[95%] xl:w-[85%] max-w-[1920px] gap-5">
           {/* Filter Component */}
-          <Filter />
-          <div className="flex flex-col justify-between min-h-full gap-5 w-full">
+          <motion.div
+            className="z-30 bottom-0 fixed  w-full lg:w-fit left-0 lg:static lg:!translate-y-0  overflow-scroll lg:overflow-visible max-h-[80%] md:max-h-[78%] lg:max-h-full"
+            initial={{ y: 800 }}
+            animate={isOpen ? { y: 0 } : { y: 800 }}
+            transition={{ ease: "easeInOut" }}
+          >
+            <Filter />
+          </motion.div>
+          <div className="flex flex-col justify-between min-h-full gap-5 w-full max-w-[920px] 2xl:max-w-[1920px]">
             <div className="w-auto">
               {/* Page Label and Products Count */}
               <div className="flex justify-between items-center mb-4">
                 <h2 className="text-3xl font-extrabold capitalize">
                   <ProductsPageLabel />
                 </h2>
-                <p>Showing 1-10 of {totalProducts} Products</p>
+                <div className="flex items-center gap-4">
+                  <p>Showing 1-10 of {totalProducts} Products</p>
+                  <div
+                    className="lg:hidden bg-default-200 rounded-full p-2"
+                    onClick={() => setIsOpen(!isOpen)}
+                  >
+                    {isOpen ? <FaTimes /> : <FaFilter />}
+                  </div>
+                </div>
               </div>
               {/* Products List */}
               <Products productsData={data?.doc} isLoading={isLoading} />
@@ -122,7 +153,9 @@ const AllProducts = () => {
                 initialPage={1}
                 total={totalPages}
                 page={page}
-                onChange={(page) => setPage(page)}
+                onChange={(page) =>
+                  updateSearchParams({ page: page }, () => setPage(page))
+                }
               />
               <Button
                 isDisabled={page >= totalPages}
